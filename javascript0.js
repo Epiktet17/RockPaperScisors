c9b19c26e06398452f03aae3fbbf8cde73e6ca88
Epiktet17 <tim.letnar@gmail.com>
2023-10-21 10:55:43 +0200

    UI aded, code cleanup

--- javascript0.js
+++ javascript0.js
@@ -1,54 +1,75 @@
-let pointComp = 0;
-let pointsPlayer = 0;
-function getComputerChoice(){
-  let min = Math.ceil(1);
-  let max = Math.floor(3);
-  compChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-  switch(compChoice){
-    case 1:
-      return "rock";
-    case 2:
-      return "paper";
-    case 3:
-      return "scisors";
-  }
-}
-function playerSelection(){
-  let choice = prompt("What will you choose: Rock, Paper or Scisors?");
-  return choice.toLowerCase();
+let computerScore = 0;
+let playerScore = 0;
+function computerChoice(){
+  const optionsRPS = ["rock","paper","scisors"];
+  let randomPick = Math.floor(Math.random() * 3);
+  return optionsRPS[randomPick];
 }
 
-function playRound(){
-  let userCh = playerSelection();
-  let computerCh = getComputerChoice();
-  if (userCh == computerCh){
-    return "It's a tie";
+
+const body = document.querySelector('body');
+const btnRock = document.createElement('button');
+btnRock.textContent = 'rock';
+btnRock.value = 'rock';
+const btnPaper = document.createElement('button');
+btnPaper.textContent = 'paper';
+btnPaper.value = 'paper';
+const btnScisors = document.createElement('button');
+btnScisors.textContent = 'scisors';
+btnScisors.value = 'scisors';
+body.appendChild(btnRock);
+body.appendChild(btnPaper);
+body.appendChild(btnScisors);
+const selectAllButtons = document.querySelectorAll('button');
+const divForDisplay = document.createElement('div');
+body.appendChild(divForDisplay);
+const finalResult = document.createElement('h4');
+const liveScore = document.createElement('p');
+const liveMoves = document.createElement('p')
+
+
+selectAllButtons.forEach((button) => {
+  button.addEventListener('click',()=>{
+    playRound(button.value);
+  });
+});
+
+function displayResult(result){
+  finalResult.textContent = result;
+  divForDisplay.appendChild(finalResult);
+}
+function displayScore(){
+  divForDisplay.appendChild(liveMoves);
+  liveScore.textContent = `Player: ${playerScore} ||  Computer: ${computerScore}`;
+  divForDisplay.appendChild(liveScore);
+}
+
+function playRound(playerPick){
+  let computerPick = computerChoice();
+  displayScore();
+
+  if (playerPick == 'rock' && computerPick == 'scisors'||
+      playerPick == 'paper' && computerPick == 'rock'||
+      playerPick == 'scisors' && computerPick == 'paper') {
+      liveMoves.textContent = `Player WINS ${playerPick} beats ${computerPick}`;
+      playerScore++;
   }
-  else if (userCh == "rock" && computerCh == "scisors" || userCh == "paper" && computerCh == "rock" || userCh == "scisors" && computerCh == "rock") {
-    pointsPlayer++;
-    return "You win {userCh} beats {compChoice}";
+
+  else if (playerPick == computerPick) {
+    liveMoves.textContent = `It's a tie, you both choose ${playerPick}`;
   }
   else {
-    pointComp++;
-    return "You lose {compChoice} beats {userCh}";
+    liveMoves.textContent = `Computer WINS ${computerPick} beats ${playerPick}`;
+    computerScore++;
   }
+displayScore();
+if (computerScore == 5) {
+  result = 'The computer WINS, it was the first to get 5 points';
+  displayResult(result);
 }
-function playGame(){
-  let roundScore=0;
-  let tockRac = 0;
-  while(roundScore <=4){
-    console.log(playRound());
-    roundScore++;
-  }
-
-  if(pointComp > pointsPlayer){
-    console.log("Computer wins");
-  }
-  else if(pointComp < pointsPlayer){
-    console.log("Player wins");
-  }
-  else{
-    console.log("Tie");
-  }
+else if (playerScore == 5) {
+  result = 'You WIN, you are the first to get 5 points';
+  displayResult(result);
+}
 
 }
